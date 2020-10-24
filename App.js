4d91c96708c0ef702d0eebcc32a3d7a4cb065096
Masayuki Yamaji <afact_of_1ife@yahoo.co.jp>
2020-10-24 12:22:52 +0900

    reactnavigaton導入

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
